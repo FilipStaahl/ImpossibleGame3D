05bd82462b6f6de30b7a3ca9db486e0193c5e947
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-07 13:46:14 +0200

    PlayerController Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, 0, 0.05f);
     }
 }
