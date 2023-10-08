2289c440d1fd6f967270ae8adf296936263f2725
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-08 14:15:55 +0200

    PlayerController Jump Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.02f; // Field, not Var
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    private void FixedUpdate()
+    {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (rigidbody.velocity.y < -.1f)
+        {
+            rigidbody.AddForce(0, -10, 0);
+        }
     }
 
     // Update is called once per frame
     public void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+            rigidbody.AddForce(0, 300, 0);
+        }
+
         transform.Translate(0, 0, speed);
     }
 }
+   
