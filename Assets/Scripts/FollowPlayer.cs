d35118fbbd69e02558d0d43f13613a07dabeaf7b
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-09 11:30:09 +0200

    Fixed Error on Death

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,10 @@ public class FollowPlayer : MonoBehaviour
     private void LateUpdate()
     {
         GameObject player = GameObject.Find("Player");
+        if (player == null)
+        {
+            return;
+        }
         Vector3 target = player.transform.position + new Vector3(0, 2, -7);
         transform.position = target;
 
